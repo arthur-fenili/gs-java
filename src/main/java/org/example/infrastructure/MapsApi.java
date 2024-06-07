7252518e2c8c7a0aedc6924774b48d578e2b3b23
Vinicio Raphael Santana <vinicioraphael2012@gmail.com>
2024-06-07 09:06:47 -0300

    Adicionado api do Maps

---
+++ src/main/java/org/example/infrastructure/MapsApi.java
@@ -0,0 +1,31 @@
+package org.example.infrastructure;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.GeocodingResult;
+
+import java.io.IOException;
+
+public class MapsApi {
+    public static void main(String[] args) {
+        String apiKey = "AIzaSyC9l-5BoiebqgQ6-EZAaORR4fDtjN9KoUs";
+
+        GeoApiContext context = new GeoApiContext.Builder()
+                .apiKey(apiKey)
+                .build();
+
+        try {
+            GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway, Mountain View, CA").await();
+
+            for (GeocodingResult result : results) {
+                System.out.println(result.formattedAddress);
+                System.out.println("Latitude: " + result.geometry.location.lat);
+                System.out.println("Longitude: " + result.geometry.location.lng);
+            }
+        } catch (ApiException | InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
